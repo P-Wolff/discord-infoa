5982550397969dfd05d30e0e92cbdf11db64dd0c
P-Wolff <priscilawolff@hotmail.com.br>
2024-07-22 16:34:02 -0300

    alteracao

---
+++ discord/src/pages/app/index.jsx
@@ -0,0 +1,29 @@
+import './index.scss'
+
+export default function App() {
+    return (
+        <div className="pagina-app">
+
+            <img src="https://cdn.discordapp.com/discovery-splashes/932076533386993754/5b776aa9ffd1038a307da72f491d64a1.jpg?size=2048" alt="" width={250}/>
+
+            <div className="secao1">
+                <div className='secao1-img-h1'>
+                    <img src="https://cdn.discordapp.com/icons/932076533386993754/4868161c27942773253b10f445e00d8d.jpg?size=256" alt="" />
+
+                    <h1>Stardew Valley Expanded</h1>  
+
+                </div>
+
+                <div className='info-baixo-titulo'>
+                    <p>This discord is for news,<br></br> discussion, and the community of Stardew Valley Expanded, an expansive mod for Stardew Valley.</p>
+                </div>
+                
+
+                <div className='paragrafo-bolinha'></div>
+                <p>7.908 Online</p>
+                <p>30.804 Members</p>
+
+            </div>
+        </div>
+    );
+}
